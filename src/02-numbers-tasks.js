7ffd0c8abd159814fa9863991a7494e86a1c78c2
unknown <ilya-1974@yandex.ru>
2020-04-27 16:45:49 +0500

    feat: create solution for vector task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -116,8 +116,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const scaleMult = x1 * x2 + y1 * y2;
+  const modVector1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const modVector2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  return scaleMult / (modVector1 * modVector2);
 }
 
 /**

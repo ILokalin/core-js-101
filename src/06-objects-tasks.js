be6126275fc00fa0d6ad112e8194174f7a2156fc
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-05-18 00:54:11 +0500

    feat: add solutions

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,15 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  function getArea() {
+    return this.width * this.height;
+  }
+  return {
+    getArea,
+    width,
+    height,
+  };
 }
 
 
@@ -35,8 +42,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +58,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const objResult = JSON.parse(json);
+  Object.setPrototypeOf(objResult, proto);
+  return objResult;
 }
 
 
@@ -111,33 +120,47 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  element: undefined,
+  id: undefined,
+  class: [],
+  attribute: [],
+  orderSequence: ['element', 'id', 'class', 'attribute', 'pseudo-class', 'pseudo-element', 'combine'],
+  element(value) {
+    this.element = value;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    this.id = value;
+    return this;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    this.class.push(`.${value}`);
+    return this;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    this.attribute.push(value);
+    return this;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    this.pseudoClass.push(`:${value}`);
+    return this;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    this.pseudoElement = value
+    return this;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return this;
   },
+
+  stringify() {
+    const classList = this.class.join('');
+  }
 };
 
 
--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -62,8 +62,25 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  const { length } = args;
+
+  return (x) => {
+    const [a, b, c] = args;
+    if (length === 3) {
+      return a * x ** 2 + b * x + c;
+    }
+
+    if (length === 2) {
+      return a * x + b;
+    }
+
+    if (length === 1) {
+      return a;
+    }
+
+    return null;
+  };
 }
 
 
@@ -81,8 +98,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const memoResult = func();
+  return () => memoResult;
 }
 
 
@@ -101,8 +119,22 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let tryAttemts = attempts;
+
+  const retryer = () => {
+    if (tryAttemts > 0) {
+      try {
+        return func();
+      } catch (e) {
+        tryAttemts -= 1;
+        return retryer();
+      }
+    }
+    return null;
+  };
+
+  return retryer;
 }
 
 
@@ -129,8 +161,16 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argumentsList = JSON.stringify(args).slice(1, -1);
+
+    logFunc(`${func.name}(${argumentsList}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argumentsList}) ends`);
+
+    return result;
+  };
 }
 
 
@@ -147,8 +187,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn.call(null, ...args1, ...args2);
 }
 
 
@@ -169,8 +209,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = -1;
+
+  return () => {
+    count += 1;
+    return startFrom + count;
+  };
 }
 
 
